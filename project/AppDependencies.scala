c0e8885d616accaf956d5867f3d6310fb65dab23
stephenheath101 <98922374+stephenheath101@users.noreply.github.com>
2023-08-21 09:23:04 +0100

    DDCE-4441 - Mongo update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion = "0.74.0"
-  val bootstrapVersion = "7.20.0"
+  private lazy val mongoHmrcVersion = "1.3.0"
+  val bootstrapVersion = "7.21.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.16.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion
   )
@@ -18,12 +18,11 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"     % mongoHmrcVersion,
     "org.scalatest"          %% "scalatest"                   % "3.2.16",
-    "org.scalatestplus.play" %% "scalatestplus-play"          % "5.1.0",
-    "org.scalatestplus"      %% "scalatestplus-scalacheck"    % "3.1.0.0-RC2",
+    "org.scalatestplus"      %% "mockito-4-11"                % "3.2.16.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.16.0",
     "wolfendale"             %% "scalacheck-gen-regexp"       % "0.1.2",
     "org.jsoup"              %  "jsoup"                       % "1.16.1",
-    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current,
-    "org.scalatestplus"      %% "mockito-4-6"                 % "3.2.15.0",
+    "org.mockito"            %% "mockito-scala-scalatest"     % "1.17.14",
     "com.github.tomakehurst" % "wiremock-standalone"          % "2.27.2",
     "com.vladsch.flexmark"   % "flexmark-all"                 % "0.64.8"
   ).map(_ % Test)
