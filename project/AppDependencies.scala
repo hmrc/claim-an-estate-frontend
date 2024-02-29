4a46041a1be04c8daba47374d8864fdcecdcb2f3
garytate123 <garytatelufc@gmail.com>
2024-02-29 17:06:41 +0000

    DDCE-5381 Dependency updates for Tudor crown change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion
   )
@@ -16,13 +16,13 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"     % mongoHmrcVersion,
-    "org.scalatest"          %% "scalatest"                   % "3.2.16",
-    "org.scalatestplus"      %% "mockito-4-11"                % "3.2.16.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.16.0",
+    "org.scalatest"          %% "scalatest"                   % "3.2.18",
+    "org.scalatestplus"      %% "mockito-4-11"                % "3.2.18.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.18.0",
     "wolfendale"             %% "scalacheck-gen-regexp"       % "0.1.2",
-    "org.jsoup"              %  "jsoup"                       % "1.16.1",
-    "org.mockito"            %% "mockito-scala-scalatest"     % "1.17.14",
-    "com.github.tomakehurst" % "wiremock-standalone"          % "2.27.2",
+    "org.jsoup"              %  "jsoup"                       % "1.17.2",
+    "org.mockito"            %% "mockito-scala-scalatest"     % "1.17.30",
+    "com.github.tomakehurst" % "wiremock-standalone"          % "3.0.1",
     "com.vladsch.flexmark"   % "flexmark-all"                 % "0.64.8"
   ).map(_ % Test)
 
