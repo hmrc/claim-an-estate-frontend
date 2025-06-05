29cb58f2940f36eee8a9fd3c13ca78e88601512e
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-06-05 12:14:33 +0100

    DDCE-6603 - updating instances with Manage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   private lazy val mongoHmrcVersion = "2.6.0"
-  private lazy val bootstrapVersion = "9.12.0"
+  private lazy val bootstrapVersion = "9.13.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.3.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
