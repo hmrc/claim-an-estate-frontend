ec8044d77a732e60ea771c9a7881b318374547ed
Mithra Balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-12-22 14:52:31 +0000

    DDCE-7764 - enabling service navigation component (#92)
    * DDCE-7764 - enabling service navigation component
    
    * DDCE-7764 - fixing test cases with service nav component
    
    * DDCE-7764 - pr bot comments
    
    * DDCE-7764 - updating dependency for fixing sign out link

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val mongoHmrcVersion = "2.10.0"
-  private lazy val bootstrapVersion = "10.2.0"
+  private lazy val mongoHmrcVersion = "2.11.0"
+  private lazy val bootstrapVersion = "10.4.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.18.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
 
@@ -16,8 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % mongoHmrcVersion,
     "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.18.0",
-    "wolfendale"             %% "scalacheck-gen-regexp"       % "0.1.2",
-    "org.jsoup"              %  "jsoup"                       % "1.21.1"
+    "wolfendale"             %% "scalacheck-gen-regexp"       % "0.1.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
