7e6782bf6a489218f0b6016478bce4d7a84d90f6
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-07-02 15:10:30 +0100

    DDCE-8576 Upgrade play-frontend-hmrc

--- app/models/package.scala
+++ app/models/package.scala
@@ -113,7 +113,6 @@ package object models {
       }
     }
 
-    // scalastyle:off
     def remove(path: JsPath): JsResult[JsValue] =
 
       (path.path, jsValue) match {
@@ -148,7 +147,6 @@ package object models {
                 }
             }
       }
-    // scalastyle:on
 
   }
 
