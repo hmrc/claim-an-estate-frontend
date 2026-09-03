60b12806b05ed4e8293c6b81abf7927304ba4ddc
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2026-09-03 14:18:04 +0100

    DCE-9167 Add service navigation to feedback URL and dependencies update

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -27,7 +27,7 @@ class FrontendAppConfig @Inject() (val configuration: Configuration, contactFron
   lazy val serviceName: String = configuration.get[String]("serviceName")
 
   val betaFeedbackUrl =
-    s"${contactFrontendConfig.baseUrl.get}/contact/beta-feedback?service=${contactFrontendConfig.serviceId.get}&useServiceNavigation"
+    s"${contactFrontendConfig.baseUrl.get}/contact/beta-feedback?service=${contactFrontendConfig.serviceId.get}"
 
   lazy val estatesRegistration: String = configuration.get[String]("urls.estatesRegistration")
   lazy val authUrl: String             = configuration.get[Service]("auth").baseUrl
